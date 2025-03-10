650fc968d2cdbac8d1faeafd45f92e30f209e887
Mark Felder <feld@feld.me>
2025-03-10 14:49:09 -0700

    Make log retention configurable

--- recipes/journald.rb
+++ recipes/journald.rb
@@ -4,10 +4,11 @@
 #
 # Copyright:: 2023, The Authors, All Rights Reserved.
 
-cookbook_file '/etc/systemd/journald.conf' do
+template '/etc/systemd/journald.conf' do
   owner 0
   group 0
   mode '0644'
+  variables({ 'config' => node['chatmail'] })
   notifies :restart, 'service[systemd-journald.service]', :immediately
 end
 
