b8bdb332fa95bd91e08f98cb0f6a8535b021d6fb
Mark Felder <feld@feld.me>
2025-04-02 00:51:41 -0700

    Combine these ruby_block statements

--- recipes/zone.rb
+++ recipes/zone.rb
@@ -4,17 +4,12 @@
 #
 # Copyright:: 2023, The Authors, All Rights Reserved.
 
-ruby_block 'generate dkim txt value' do
+ruby_block 'generate dkim txt value and sts_id' do
   block do
     dkim_pubkey = %x(openssl rsa -in /etc/dkimkeys/opendkim.private -pubout 2>/dev/null | awk '/-/{next}{printf("%s",$0)}')
     dkim_txt_raw = "v=DKIM1;k=rsa;p=#{dkim_pubkey};s=email;t=s"
     dkim_txt_value = dkim_txt_raw.scan(/.{1,255}/).map { |chunk| "\"#{chunk}\"" }.join(' ')
     node.override['dkim_txt_value'] = dkim_txt_value
-  end
-end
-
-ruby_block 'read the sts_id value' do
-  block do
     node.override['sts_id'] = File.stat('/var/www/html/.well-known/mta-sts.txt').mtime.to_i
   end
 end
